da23c7532f8c2d278663979583a54b4cdf7d692c
othman-19 <othmane@naiein.com>
2023-06-23 14:22:55 +0100

    Added a vec of threads to the threadpool struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-pub struct ThreadPool;
-
+use std::thread;
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 impl ThreadPool {
   /// Create a new ThreadPool.
   ///
@@ -11,11 +13,18 @@ impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
-    ThreadPool
-  }
-    pub fn execute<F>(&self, f: F)
-    where
-        F: FnOnce() + Send + 'static,
-    {
+    let mut threads = Vec::with_capacity(size);
+
+    for _ in 0..size {
+      // create some threads and store them in the vector
     }
+    
+    ThreadPool { threads }
+  }
+  
+  pub fn execute<F>(&self, f: F)
+  where
+      F: FnOnce() + Send + 'static,
+  {
+  }
 }
